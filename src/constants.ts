bcd601449a3e84d23fd7937919031190e3e65df8
PedroNestor2078 <pedro.nestor@temsaude.com>
2024-01-10 21:34:39 -0300

    hotfix: favicon and https in api

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const DESKTOP_IMAGE_URL = 'https://picsum.photos/432/230';
 export const MOBILE_IMAGE_URL = 'https://picsum.photos/115/130';
 
-export const PEOPLE_URL = 'http://swapi.dev/api/people/';
-export const PLANETS_URL = 'http://swapi.dev/api/planets';
+export const PEOPLE_URL = 'https://swapi.dev/api/people/';
+export const PLANETS_URL = 'https://swapi.dev/api/planets';
